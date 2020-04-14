874b38e2f6be6734c4749610744516ad0dfae6f6
alaaseada <51153316+alaaseada@users.noreply.github.com>
2020-04-14 13:14:06 +0200

    Update create.sh

--- Infra/nodes/create.sh
+++ Infra/nodes/create.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-aws cloudformation create-stack --stack-name "eks-nodes" --template-body file://nodes.yml --parameters file://nodes-params.json --capabilities CAPABILITY_NAMED_IAM --region=us-west-2
+
+sed "s/replaceclustername/$1/g" nodes-params.json
+
+aws cloudformation create-stack --stack-name "eks-nodes" \
+--template-body file://nodes.yml \
+--parameters file://nodes-params.json \
+--capabilities CAPABILITY_NAMED_IAM \
+--region=us-west-2
