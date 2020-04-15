79835394c2c71a83dd3c78bc23557799511f9942
Ubuntu <ubuntu@ip-172-31-24-197.us-west-2.compute.internal>
2020-04-15 12:14:31 +0000

    Update

--- infra/vpc/create.sh
+++ infra/vpc/create.sh
@@ -2,7 +2,7 @@
 
 echo "Hello world, from VPC"
 aws cloudformation create-stack --stack-name "eks-vpc" \
-	--template-body file://networkinfra.yml \
-	--parameters file://networkinfra-params.json \
+	--template-body file://networkInfra.yml \
+	--parameters file://networkInfra-params.json \
 	--capabilities CAPABILITY_NAMED_IAM \
 	--region=us-west-2
