e71e47d7055178b5a5e37de47920e976ac633aac
Ubuntu <ubuntu@ip-172-31-24-197.us-west-2.compute.internal>
2020-04-15 19:04:48 +0000

    add wait step to getClusterName untile the cluster fully created

--- infra/cluster/getClusterName.sh
+++ infra/cluster/getClusterName.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 echo "Hello world, from get cluster name script!"
+
+aws cloudformation wait stack-create-complete --region us-west-2 --stack-name "eks-cluster" 
+
 aws cloudformation describe-stacks \
 	  --stack-name "eks-cluster" \
 	  --query "Stacks[0].Outputs[?OutputKey=='eks-cluster-ClusterName'].OutputValue" \
