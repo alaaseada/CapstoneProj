775d23560216dd8c5f00e11cb1210b50c55e5b03
Ubuntu <ubuntu@ip-172-31-24-197.us-west-2.compute.internal>
2020-04-17 16:06:28 +0000

    Merge deployment and service yaml files

--- updatebranches.sh
+++ updatebranches.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 git stage .
-git commit -m "separate deployment and service yaml files"
+git commit -m "Merge deployment and service yaml files"
 git checkout development
 git pull . master
 git checkout production
